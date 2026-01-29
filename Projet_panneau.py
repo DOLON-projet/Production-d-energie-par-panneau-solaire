9379971ea1b3ef437a5e152794bdf12c82194587
mika <gmail>
2026-01-29 14:34:14 +0100

    lecture et mise d'informations dans des variables

--- Projet_panneau.py
+++ Projet_panneau.py
@@ -2,6 +2,15 @@ import json
 with open('test_transcript_json.json','r') as op:
     read_data = op.read()
     op.close()
-    print(read_data)
-True
 
+donnees = json.loads(read_data)
+tension_panneau_solaire = donnees['tP']
+tension_Batterie = donnees['tB']
+courant_panneau_solaire = donnees['cP']
+courant_Batterie = donnees['cB']
+temperature_armoire = donnees['tA']
+print(tension_panneau_solaire)
+print(tension_Batterie)
+print(courant_panneau_solaire)
+print(courant_Batterie)
+print(temperature_armoire)
