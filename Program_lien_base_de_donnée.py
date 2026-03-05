f2aaebcbb2c28edf5f78cffa6f49ba73d8b8c23b
ETUDIANT_2A <michael.d050505@gmail.com>
2026-03-05 16:42:41 +0100

    lecture et implementation des variable predefini dans la base de donnée distante de mariadb #2

--- Program_lien_base_de_donnée.py
+++ Program_lien_base_de_donnée.py
@@ -1,27 +1,26 @@
-import mariadb
-from mariadb import Error
-tension_batterie=17.5
-tension_du_panneau=12.5
+import mariadb                             #lien avec la base de donné
+from mariadb import Error                  #lien avec la bibliotèque d'erreur de mariadb
+tension_batterie=12                        #initialisation de variables d'exemple
+tension_du_panneau=10.5
 
 try:
 
-    print("Try to connected to Mariadb Server")
-    connection =  mariadb.connect(host='172.20.30.13',
-                                  database='projet_pedagogique',
-                                  user="admin",
-                                  password="fourcade")
+    print("Try to connected to Mariadb Server")                    #affichage textuelle de la connection à mariadb
+    connection =  mariadb.connect(host='172.20.30.13',             #adresse de l'hôte
+                                  database='projet_pedagogique',   #connection a la base de donné "projet_pedagogique"
+                                  user="admin",                    #nom de l'utulisateur "admin"
+                                  password="fourcade")             #mot de passe "fourcade"
 
-    cursor = connection.cursor()
+    cursor = connection.cursor()          #création du de l'objet "cursor" qui permet la modification
 
-    cursor.execute("INSERT INTO info_panneau (tension_batterie,tension_du_panneau) VALUES (?,?)" , (tension_batterie, tension_du_panneau))
+    cursor.execute("INSERT INTO info_panneau (tension_batterie,tension_du_panneau) VALUES (?,?)" , (tension_batterie, tension_du_panneau))          #utilisation du cursor qui permet la creation des variable de temp etc...
     connection.commit()
 
-    if connection.server_status:
+    if connection.server_status:        #si la connection est fini alors fermer le curseur et la connection
         cursor.close()
         connection.close()
-        print("Mariadb connection is closed")
 
 
 except Error as e:
-    print("Error while connecting to Mariadb", e)
+    print("Error while connecting to Mariadb", e)      #si il y a une erreur à la connection à mariadb, alors afficher "Error while connecting to Mariadb"
 
