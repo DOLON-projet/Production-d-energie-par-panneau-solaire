11c156cd955c4b572fb1f9846615a50434f66570
ETUDIANT_2A <michael.d050505@gmail.com>
2026-03-05 17:01:36 +0100

    lecture et implementation des variable predefini dans la base de donnée distante de mariadb #2

--- Projet_panneau.py
+++ Projet_panneau.py
@@ -1,8 +1,9 @@
-import json
-with open('test_transcript_json.json','r') as op:
-    read_data = op.read()
-    op.close()
+import json          #importation de la bibliotèque json
+with open('test_transcript_json.json','r') as op:         #ouverture des donné dans le fichier json
+    read_data = op.read()            #creation de variable qui lit les données
+    op.close()             #fermeture du fichier json
 
+#mise en place des données dans les variables correspondantes
 donnees = json.loads(read_data)
 tension_panneau_solaire = donnees['tP']
 tension_Batterie = donnees['tB']
