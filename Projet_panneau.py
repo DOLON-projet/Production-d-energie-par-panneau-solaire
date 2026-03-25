51a7d96d9881384a855d22f325e545bd8ec7696d
ETUDIANT_2A <michael.d050505@gmail.com>
2026-03-25 17:01:04 +0100

    lecture et implementation des variable predefini dans la base de donnée distante de mariadb et prise de mot de passe dans le serveur

--- Projet_panneau.py
+++ Projet_panneau.py
@@ -1,17 +1,20 @@
-import json          #importation de la bibliotèque json
-with open('test_transcript_json.json','r') as op:         #ouverture des donné dans le fichier json
-    read_data = op.read()            #creation de variable qui lit les données
-    op.close()             #fermeture du fichier json
-
-#mise en place des données dans les variables correspondantes
-donnees = json.loads(read_data)
-tension_panneau_solaire = donnees['tP']
-tension_Batterie = donnees['tB']
-courant_panneau_solaire = donnees['cP']
-courant_Batterie = donnees['cB']
-temperature_armoire = donnees['tA']
-print("tension panneau : ",tension_panneau_solaire)
-print("tension baterie : ",tension_Batterie)
-print("courant panneau solaire : ",courant_panneau_solaire)
-print("courant batterie : ",courant_Batterie)
-print("temperature armoire : ",temperature_armoire)
+import requests
+import time
+import json   #importation de la bibliotèque json
+while True:
+    with open('test_transcript_json.json','r') as op:
+        for line in op:
+            # mise en place des données dans les variables correspondantes
+            donnees = json.loads(line)
+            tension_panneau_solaire = donnees['tP']
+            tension_Batterie = donnees['tB']
+            courant_panneau_solaire = donnees['cP']
+            courant_Batterie = donnees['cB']
+            temperature_armoire = donnees['tA']
+            print("tension panneau : ", tension_panneau_solaire)
+            print("tension baterie : ", tension_Batterie)
+            print("courant panneau solaire : ", courant_panneau_solaire)
+            print("courant batterie : ", courant_Batterie)
+            print("temperature armoire : ", temperature_armoire)
+            requests.post()
+    time.sleep(10)
